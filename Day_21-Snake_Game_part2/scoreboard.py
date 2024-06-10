5981a88848ee658a09a0c473ae8c8ecf92cca75a
r0back55 <r0back55@gmail.com>
2024-06-10 20:37:04 +0200

    New features

--- Day_21-Snake_Game_part2/scoreboard.py
+++ Day_21-Snake_Game_part2/scoreboard.py
@@ -9,6 +9,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt", mode="r") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.penup()
         self.goto(x=0, y=260)
@@ -16,13 +18,21 @@ class Scoreboard(Turtle):
         self.update_score()
 
     def update_score(self):
-        self.write(f"SCORE: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score}        High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align="center", font=('Courier', 24, 'normal'))
+    def reset_scoreboard(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as new_file:
+                new_file.write(f"{self.high_score}")
+        self.score = 0
+        self.update_score()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align="center", font=('Courier', 24, 'normal'))
 
     def counter(self):
         self.score += 1
-        self.clear()
         self.update_score()
