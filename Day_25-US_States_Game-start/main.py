0d3c23979fc23f7e039510e5c3972e58b32ed003
r0back55 <r0back55@gmail.com>
2024-06-14 18:09:50 +0200

    changes within loops

--- Day_25-US_States_Game-start/main.py
+++ Day_25-US_States_Game-start/main.py
@@ -15,6 +15,11 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States",
                                     prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        to_learn = [state for state in all_states if state not in guessed_states]
+        new_data = pd.DataFrame(to_learn)
+        new_data.to_csv("states_to_learn.csv")
+        break
 
     if answer_state in all_states:
         guessed_states.append(answer_state)
@@ -24,5 +29,3 @@ while len(guessed_states) < 50:
         state_data = data[data["state"] == answer_state]
         t.goto(int(state_data.x.iloc[0]), int(state_data.y.iloc[0]))
         t.write(state_data.state.item())
-
-screen.exitonclick()
