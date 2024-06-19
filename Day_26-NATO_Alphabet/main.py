8ac6d42a04d88781ddf8c73cbe9459ec436e41fc
r0back55 <r0back55@gmail.com>
2024-06-19 16:50:17 +0200

    Small upgrade with error handling

--- Day_26-NATO_Alphabet/main.py
+++ Day_26-NATO_Alphabet/main.py
@@ -9,5 +9,24 @@ nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 user_input = input("Type a word:  ").upper()
-fonetic_list = [nato_dict[letter] for letter in user_input]
-print(fonetic_list)
+
+try:
+    fonetic_list = [nato_dict[letter] for letter in user_input]
+except KeyError:
+    print("Sorry, only letters in the alphabet please")
+    user_input = input("Type a word:  ").upper()
+else:
+    print(fonetic_list)
+
+
+# Or we can crete a function:
+"""
+def generate_fonetic_list():
+    try:
+        fonetic_list = [nato_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_fonetic_list()
+    else:
+        print(fonetic_list)
+"""
